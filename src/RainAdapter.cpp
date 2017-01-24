f6735ed047626b015bad40c595fbd202505ad471
Gerald Dachs <gda@dachsweb.de>
2017-01-24 15:35:41 +0100

    added read thread

--- src/RainAdapter.cpp
+++ src/RainAdapter.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include "serialport.h"
 #include <p8-platform/util/util.h>
 
@@ -5,19 +7,89 @@
 
 using namespace P8PLATFORM;
 
-RainAdapter::RainAdapter(const char *deviceName)
+RainAdapter::RainAdapter(const char *deviceName) :
+        m_gotResponse(false)
+
 {
     CLockObject lock(m_mutex);
-    m_port = new CSerialPort(deviceName, 115200);
+    m_port = new CSerialPort(deviceName, CEC_RAINSHADOW_SERIAL_DEFAULT_BAUDRATE);
 }
 
 RainAdapter::~RainAdapter(void)
 {
-  /* stop the reader thread */
-  StopThread(0);
+    /* stop the reader thread */
+    StopThread(0);
+}
+bool RainAdapter::IsOpen(void)
+{
+  /* thread is not being stopped, the port is open and the thread is running */
+  return !IsStopped() && m_port->IsOpen() && IsRunning();
 }
 
 void *RainAdapter::Process(void)
 {
-  return 0;
+    char buf[DATA_SIZE];
+    unsigned int idx = 0;
+    bool started = false;
+    unsigned char data;
+
+    if (!IsOpen())
+        return 0;
+
+    while (!IsStopped())
+    {
+        do
+        {
+            /* retry Read() if it was interrupted */
+            m_port->Read(&data, sizeof(uint8_t), 50);
+        }
+        while (m_port->GetErrorNumber() == EINTR);
+
+        if (m_port->GetErrorNumber())
+        {
+            std::cerr << "No permission to open the device" << std::endl;
+
+            break;
+        }
+
+        if (!started && data != '?')
+            continue;
+        if (data == '\r')
+        {
+            buf[idx] = '\0';
+
+            if (!strncmp(buf, "REC", 3))
+                std::cout << buf << std::endl;
+            else
+            {
+                strncpy(m_response, buf, sizeof(m_response));
+
+                m_gotResponse = true;
+                m_condition.Signal();
+            }
+            idx = 0;
+            started = false;
+            continue;
+        }
+        else if (data == '?')
+        {
+            idx = 0;
+            started = true;
+            continue;
+        }
+
+        if (data == '\n')
+        {
+            idx = 0;
+            started = false;
+            continue;
+        }
+        if (idx >= DATA_SIZE - 1)
+        {
+            idx = 0;
+        }
+        buf[idx++] = data;
+        continue;
+    }
+    return 0;
 }
--- src/RainAdapter.h
+++ src/RainAdapter.h
@@ -4,6 +4,9 @@
 #include <p8-platform/threads/threads.h>
 #include <p8-platform/sockets/socket.h>
 
+#define DATA_SIZE 256
+#define CEC_RAINSHADOW_SERIAL_DEFAULT_BAUDRATE 115200L
+
 class RainAdapter: public P8PLATFORM::CThread
 {
 public:
@@ -16,9 +19,13 @@ public:
     void *Process(void);
     ///}
 private:
-    P8PLATFORM::ISocket * m_port; /**< the com port connection */
-    P8PLATFORM::CMutex m_mutex;
+    bool IsOpen(void);
 
+    P8PLATFORM::ISocket *m_port; /**< the com port connection */
+    P8PLATFORM::CMutex m_mutex;
+    char                          m_response[DATA_SIZE]; /**< current response from adapter */
+    P8PLATFORM::CCondition<bool>  m_condition;
+    bool                          m_gotResponse;
 };
 
 #endif //RAINADAPTER_H_
